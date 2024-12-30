9b42589e509f5f3cbbeaefea59e8b28280bea121
wilmitch <mwangimwilfred@gmail.com>
2024-12-30 23:01:32 +0300

    Added more features for registration including profile picture

--- client/src/pages/RegisterPage.jsx
+++ client/src/pages/RegisterPage.jsx
@@ -1,26 +1,52 @@
-import React from 'react'
+import React, { useEffect, useState } from "react";
+import "../styles/Register.scss"
 
 const RegisterPage = () => {
-  return (
-    <div className='register'>
-        <div className='register_content'>
-            <form>
-                <input placeholder='First Name' name='firstname' required/>
-                <input placeholder='Last Name' name='lastname' required/>
-                <input placeholder='Email' name='email' type='email' required />
-                <input placeholder='Password' name='password' type='password' required/>
-                <input placeholder='Confirm Password' name='confirmPassword' type='password' required/>
-                <input id='Image' type='file' name='profileImage' accept='Image/*' required/>
-                <label htmlFor="Image">
-                    <img src="/assets/addImage.png" alt="add profile pic" />
-                    <p>Upload Your Photo</p>
-                </label>
-                <button type='submit'>REGISTER</button>
-            </form>
-            <a href="/login">Already have an account? Log in here</a>
+    const [formData, setFormData] = useState({
+        firstName: "",
+        lastName: "",
+        email: "",
+        password: "",
+        confirmPassword: "",
+        profileImage: null,
+    })
+
+    const handleChange = (e) => {
+        const { name, value, files } = e.target
+        setFormData({
+            ...formData,
+            [name]: value,
+            [name]: name == "profileImage" ? files[0] : value
+        })
+    }
+    console.log(formData)
+
+    return (
+        <div className='register'>
+            <div className='register_content'>
+                <form className='register_content_form'>
+                    <input placeholder='First Name' name='firstName' value={formData.firstName} onChange={handleChange} required />
+                    <input placeholder='Last Name' name='lastName' value={formData.lastName} onChange={handleChange} required />
+                    <input placeholder='Email' name='email' type='email' value={formData.email} onChange={handleChange} required />
+                    <input placeholder='Password' name='password' type='password' value={formData.password} onChange={handleChange} required />
+                    <input placeholder='Confirm Password' name='confirmPassword' type='password' value={formData.confirmPassword} onChange={handleChange} required />
+                    <input id='Image' type='file' name='profileImage' accept='Image/*' style={{ display: "none" }} value={formData.profileImage} onChange={handleChange} required />
+                    <label htmlFor="Image">
+                        <img src="/assets/addImage.png" alt="add profile pic" />
+                        <p>Upload Your Photo</p>
+                    </label>
+                    
+                    {formData.profileImage && (
+                        <img src={URL.createObjectURL(formData.profileImage)}
+                        alt="profile photo" style={{ maxWidth: "80px" }}
+                        />
+                    )}
+                    <button type='submit'>REGISTER</button>
+                </form>
+                <a href="/login">Already have an account? Log in here</a>
+            </div>
         </div>
-    </div>
-  )
+    )
 }
 
 export default RegisterPage
