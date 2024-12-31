f095edeca300688a6646eb565c7f246ff8640895
wilmitch <mwangimwilfred@gmail.com>
2024-12-31 16:01:53 +0300

    Linked with router and added auth as well

--- server/index.js
+++ server/index.js
@@ -4,9 +4,14 @@ const mongoose = require("mongoose");
 const dotenv = require("dotenv").config();
 const cors = require("cors");
 
+const authRoutes = require("./routes/auth.js")
+
 app.use(cors());
 app.use(express.json());
 app.use(express.static("public"));
+
+/* routes */
+app.use("/auth", authRoutes)
 
 /* mongoosetup */
 const PORT = 5001;
