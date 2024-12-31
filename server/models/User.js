ccc0d4cbb2139c4463a451c8d965f787ece2af7b
wilmitch <mwangimwilfred@gmail.com>
2024-12-31 15:12:14 +0300

    my users schema

---
+++ server/models/User.js
@@ -0,0 +1,27 @@
+const mongoose = require("mongoose")
+
+const UserSchema = new mongoose.Schema(
+    {
+        firstName: {
+            type: String,
+            required: true,
+        },
+        lastName: {
+            type: String,
+            required: true,
+        },
+        Email: {
+            type: String,
+            required: true,
+            unique: true,
+        },
+        password: {
+            type: String,
+            required: true
+        },
+        profileImagePath: {
+            type: String,
+            default: "",
+        }
+    }
+)
