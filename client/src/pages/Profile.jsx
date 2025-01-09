3b115e27ca7a7aff9452570a53980896d477f668
wilmitch <mwangimwilfred@gmail.com>
2025-01-09 14:11:40 +0300

    created the 'Update user' server route

--- client/src/pages/Profile.jsx
+++ client/src/pages/Profile.jsx
@@ -1,6 +1,8 @@
 import { useSelector } from 'react-redux';
+import { useRef } from 'react';
 
 export default function Profile() {
+  const fileRef = useRef(null)
   const {currentUser} = useSelector((state) => state.user)
   return (
     <div className='p-3 max-w-lg mx-auto'>
@@ -8,7 +10,8 @@ export default function Profile() {
         Profile
       </h1>
       <form className='flex-col flex gap-4'>
-        <img src="https://media.istockphoto.com/id/1300845620/vector/user-icon-flat-isolated-on-white-background-user-symbol-vector-illustration.jpg?s=612x612&w=0&k=20&c=yBeyba0hUkh14_jgv1OKqIH0CCSWU_4ckRkAoy2p73o=" alt="Profile Picture"
+        <input type="file" ref={fileRef} hidden accept='image/*' />
+        <img onClick={() => fileRef.current.click()} src="https://media.istockphoto.com/id/1300845620/vector/user-icon-flat-isolated-on-white-background-user-symbol-vector-illustration.jpg?s=612x612&w=0&k=20&c=yBeyba0hUkh14_jgv1OKqIH0CCSWU_4ckRkAoy2p73o=" alt="Profile Picture"
         className='mt-2 h-24 w-26 cursor-pointer rounded-full self-center object-cover' />
         <input className='p-3 border rounded-lg' type="text" placeholder='username' id='username' />
         <input className='p-3 border rounded-lg' type="email" placeholder='email' id='email' />
