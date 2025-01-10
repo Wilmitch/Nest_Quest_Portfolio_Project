f8ed4a5b30c01685ae51500f5d612d3244c79772
wilmitch <mwangimwilfred@gmail.com>
2025-01-10 05:49:41 +0300

    About page ui

--- client/src/pages/About.jsx
+++ client/src/pages/About.jsx
@@ -2,6 +2,13 @@ import React from 'react'
 
 export default function About() {
   return (
-    <div>About</div>
+    <div className='py-20 px-4 max-w-6xl mx-auto'>
+      <h1 className='text-3xl font-bold mb-4 text-slate-800'>About Nest Quest</h1>
+      <p className='mb-4 text-slate-700'>Nest Quest is a first of its kind web application that specializes in helping clients find their next rental homes at the comfort of their soon to be 'previous home'. This web application is meant to provide a smooth as possible transition to that new home.</p>
+      <p className='mb-4 text-slate-700'>
+      The mission is to help our users accomplish their home quest by providing expert insights, location, adequate information in regards to the properties, and a deep understanding of the local market. Whether you are looking to expensive or a fairly priced property, we are here to help you every step of the way.
+      </p>
+      <p className='mb-4 text-slate-700'>With a design thats meant for both tenants and landlords, each of these users can access information relevant to them and that will benefit them in either home quest or finding ideal tenants for their properties. We believe that renting a property should be an exciting and rewarding experience, and we are dedicated to making that a reality for each and every one of our users.</p>
+    </div>
   )
 }
