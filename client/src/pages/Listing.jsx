360343c19fcfacc9eee360c1f1ae222b18e2d877
wilmitch <mwangimwilfred@gmail.com>
2025-01-10 05:35:17 +0300

    added chat with owner functionality on the listong page

--- client/src/pages/Listing.jsx
+++ client/src/pages/Listing.jsx
@@ -90,24 +90,19 @@ export default function Listing() {
           <div className='flex flex-col max-w-4xl mx-auto p-3 my-7 gap-4'>
             <p className='text-2xl font-semibold'>
               {listing.name} - ${' '}
-              {listing.offer
+                {listing.className 
                 ? listing.discountPrice.toLocaleString('en-US')
                 : listing.regularPrice.toLocaleString('en-US')}
               {listing.type === 'rent' && ' / month'}
             </p>
             <p className='flex items-center mt-6 gap-2 text-slate-600  text-sm'>
               <FaMapMarkerAlt className='text-green-700' />
-              {listing.address}
+              {listing.location}
             </p>
             <div className='flex gap-4'>
               <p className='bg-red-900 w-full max-w-[200px] text-white text-center p-1 rounded-md'>
-                {listing.type === 'rent' ? 'For Rent' : 'For Sale'}
+                {listing.type === 'deposit' ? 'For Rent' : 'For Sale'}
               </p>
-              {listing.offer && (
-                <p className='bg-green-900 w-full max-w-[200px] text-white text-center p-1 rounded-md'>
-                  ${+listing.regularPrice - +listing.discountPrice} OFF
-                </p>
-              )}
             </div>
             <p className='text-slate-800'>
               <span className='font-semibold text-black'>Description - </span>
@@ -132,7 +127,7 @@ export default function Listing() {
               </li>
               <li className='flex items-center gap-1 whitespace-nowrap '>
                 <FaChair className='text-lg' />
-                {listing.furnished ? 'Furnished' : 'Unfurnished'}
+                {listing.deposit ? 'Deposit Required' : 'Deposit Not Required'}
               </li>
             </ul>
             {currentUser && listing.userRef !== currentUser._id && !contact && (
