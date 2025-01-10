70a5be195fa96b9a44d3ad0d81c2b7ae127ddc7d
wilmitch <mwangimwilfred@gmail.com>
2025-01-10 05:16:21 +0300

    added swiper to my images as slider

--- api/index.js
+++ api/index.js
@@ -5,6 +5,7 @@ import UserRouter from './routes/user.route.js';
 import authRouter from './routes/auth.route.js';
 import listingRouter from './routes/listing.route.js';
 import cookieParser from 'cookie-parser';
+import path from 'path';
 dotenv.config();
 
 mongoose
@@ -15,6 +16,8 @@ mongoose
     .catch((err) => {
         console.log(err);
     });
+
+    const __dirname = path.resolve();
 
 const app = express();
 
@@ -29,12 +32,18 @@ app.use('/api/user', UserRouter);
 app.use('/api/auth', authRouter);
 app.use('/api/listing', listingRouter);
 
+app.use(express.static(path.join(__dirname, '/client/dist')));
+
+app.get('*', (req, res) => {
+  res.sendFile(path.join(__dirname, 'client', 'dist', 'index.html'));
+})
+
 app.use((err, req, res, next) => {
-    const statusCode = err.statusCode || 500;
-    const message = err.message || 'Internal Server Error';
-    return res.status(statusCode).json({
-        success: false,
-        statusCode,
-        message,
-    });
+  const statusCode = err.statusCode || 500;
+  const message = err.message || 'Internal Server Error';
+  return res.status(statusCode).json({
+    success: false,
+    statusCode,
+    message,
+  });
 });
