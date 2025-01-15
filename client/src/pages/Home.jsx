dcee850c312651633a8c812acb790dd51d6b9581
wilmitch <mwangimwilfred@gmail.com>
2025-01-15 21:43:04 +0300

    Added the swiper to navigate through offer listing with swipe ability, listing results for rental properties, and sale listing on the home page

--- client/src/pages/Home.jsx
+++ client/src/pages/Home.jsx
@@ -50,15 +50,15 @@ export default function Home() {
       {/* top */}
       <div className='flex flex-col gap-6 p-28 px-3 max-w-6xl mx-auto'>
         <h1 className='text-slate-700 font-bold text-3xl lg:text-6xl'>
-          Find your next <span className='text-slate-500'>perfect</span>
+          Find your next <span className='text-slate-500'>Amazing Home</span>
           <br />
-          place with ease
+          with ease, at the comfort of your soon to be 'Previous house'
         </h1>
-        <div className='text-gray-400 text-xs sm:text-sm'>
-          Sahand Estate is the best place to find your next perfect place to
-          live.
+        <div className='text-gray-600 text-xs sm:text-sm'>
+          Nest Quest is the best online platform for you to find your next perfect home to
+          live in.
           <br />
-          We have a wide range of properties for you to choose from.
+          We have a wide range of listed properties for you to choose from.
         </div>
         <Link
           to={'/search'}
